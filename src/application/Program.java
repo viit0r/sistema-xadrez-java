a170e1dc37f871cae9b36071e7002e9fc5a8073c
viit0r <vituuoliveira1@gmail.com>
2022-09-13 21:55:14 -0300

    Inicio da implementação de Tabuleiro e Peça

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,10 @@
 package application;
 
 import boardgame.Position;
+import boardgame.Tabuleiro;
 
 public class Program {
     public static void main(String[] args) {
-        Position position = new Position(3, 5);
-        System.out.println(position);
+        Tabuleiro tabuleiro = new Tabuleiro(8, 8);
     }
 }
---
+++ src/boardgame/Peca.java
@@ -0,0 +1,14 @@
+package boardgame;
+
+public class Peca {
+    protected Position position;
+    private Tabuleiro tabuleiro;
+
+    public Peca(Tabuleiro tabuleiro) {
+        this.tabuleiro = tabuleiro;
+    }
+
+    protected Tabuleiro getTabuleiro() {
+        return tabuleiro;
+    }
+}
